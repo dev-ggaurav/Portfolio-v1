9ed41bc4bcd91626d891bb91308badd702287ec0
devggaurav <devggaurav@gmail.com>
2020-10-21 20:50:50 +0530

    created mobile menu

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const body = document.body;
 const toggle = document.getElementById('toggle-btn');
+const hamburger = document.getElementById('hamburger');
+const menu = document.getElementById('menu');
 
 toggle.addEventListener('click',function(){
     body.classList.toggle('active');
 });
+
+
+hamburger.addEventListener('click', function() {
+    hamburger.classList.toggle('is-active');
+    menu.classList.toggle('is-active');
+})
