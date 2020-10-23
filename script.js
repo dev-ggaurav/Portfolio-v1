bb22dadea4aa66f7b9f6c259d10a4c82efe685d1
devggaurav <devggaurav@gmail.com>
2020-10-23 18:00:13 +0530

    localStore to dark mode

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 const body = document.body;
 const toggle = document.getElementById('toggle-btn');
+const currentTheme = localStorage.getItem('currentTheme');
 const hamburger = document.getElementById('hamburger');
 const menu = document.getElementById('menu');
 
 
 //function for dark theme toggle
-function theme() {
-    body.classList.toggle('active');
+if (currentTheme) {
+    body.classList.add('active');
 }
 
-toggle.addEventListener('click', theme);
+toggle.addEventListener('click', function() {
+    body.classList.toggle('active');
+    
+    if (body.classList.contains('active')) {
+        localStorage.setItem('currentTheme', 'themeActive');
+    }else{
+        localStorage.removeItem('currentTheme');
+    }
+});
 
 
 //function for mobile/hambger menu
