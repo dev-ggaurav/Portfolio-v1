11e78cb2558a479d1d16a9018ef7d2a0441a3516
devggaurav <devggaurav@gmail.com>
2020-11-02 11:20:58 +0530

    design change

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ const toggle = document.getElementById('toggle-btn');
 const currentTheme = localStorage.getItem('currentTheme');
 const hamburger = document.getElementById('hamburger');
 const menu = document.getElementById('menu');
-const triggers = menu.querySelectorAll('a');
-const highlight = document.createElement('span');
 
 //function for dark theme toggle
 if (currentTheme) {
@@ -30,16 +28,3 @@ function mobileMenu() {
 
 hamburger.addEventListener('click', mobileMenu);
 
-
-//follow along nav links
-highlight.classList.add('highlight');
-document.body.append(highlight);
-
-function highlightLink() {
-    const linkCoords = this.getBoundingClientRect();
-    highlight.style.width = `${linkCoords.width}px`;
-    highlight.style.height = `${linkCoords.height}px`;
-    highlight.style.transform = `translate(${linkCoords.left}px, ${linkCoords.top}px)`;
-}
-
-triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
